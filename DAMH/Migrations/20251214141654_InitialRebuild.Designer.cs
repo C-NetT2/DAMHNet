d72f255d0516244945c84389afbdbce96ceb57b3
Nhan <23130217@st.hcmuaf.edu.vn>
2026-01-01 11:37:05 +0700

    update

--- DAMH/Migrations/20251214141654_InitialRebuild.Designer.cs
+++ DAMH/Migrations/20251214141654_InitialRebuild.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAMH.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    [Migration("20251204140807_AddPaymentTransactions")]
-    partial class AddPaymentTransactions
+    [Migration("20251214141654_InitialRebuild")]
+    partial class InitialRebuild
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
